0d2562bb49bfbecd369ad2583ba1600581e3bec6
xavisa <j-lavis@hotmail.com>
2015-12-07 12:35:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -23,13 +23,24 @@
 						<ul>
 							<li><a href="sesionAdmin.php">Administrador</a></li>
 							<li><a href="planeacion.php" target ="cont">Planeacion de la auditoria</a></li>
+							<li><a href="AUDITORIA EQUIPO-7/index.html">Interpretacion de la Informacion</a></li>
 							<!--Aqui debes poner los links para cada tema el efecto se hace automáticamente -->
 						</ul>
 					</div>
 	</div>
 	<div id="contenido">
 	<iframe name ="cont" width="927px" height="700" scrolling="yes"></iframe>
-	<!--Aqui debes colocar todo lo relacionado a tu tema -->			
+	<!--Aqui debes colocar todo lo relacionado a tu tema -->
+	<!-- Comienzo equipo 7 -->
+	<ul>
+   <li><a href="AUDITORIA EQUIPO-7/infocap3.php"><input type="button" value="INFORMACION SOBRE CAPITULO 3" class="boton"></a>   
+   <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 4" class="boton"></a>
+   <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 5" class="boton"></a>
+   <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 6" class="boton"></a>
+   <li><a href="AUDITORIA EQUIPO-7/dictamenfinal.php"><input type="button" value="DICTAMEN FINAL" class="boton"></a>
+   <li><a href="AUDITORIA EQUIPO-7/fundamentoteoricoequipo7.php"><input type="button" value="FUNDAMENTO TEORICO EQUIPO 7" class="boton"></a>
+</ul>
+	<!-- fin equipo 7 -->
 	
 	</div>	
 	<div class="nofloat">
