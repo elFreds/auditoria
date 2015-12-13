0716e4755c12a4cfa905a227cc37be9fe3c8fe3d
xavisa <j-lavis@hotmail.com>
2015-12-13 17:50:25 -0600

    Update and rename index.html to index.php

---
+++ AUDITORIA EQUIPO-7/index.php
@@ -0,0 +1,22 @@
+
+<html>
+ <head>
+  <title>  INDEX CAPITULO 7  </title>
+  <meta http-equiv=”Content-Type” content=”text/html; charset=iso-8859-2″>
+    <link rel="stylesheet" type="text/css" href="css/css.css"/>
+ </head>
+<body style=”text-align:justify;”>
+
+<!-- Menú de navegación del sitio -->
+<center>
+<ul>
+   <li><a href="infocap3.php"><input type="button" value="INFORMACION SOBRE CAPITULO 3"></a>   
+   <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 4"></a>
+   <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 5"></a>
+   <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 6"></a>
+   <li><a href="dictamenfinal.php"><input type="button" value="DICTAMEN FINAL></a>
+   <li><a href="fundamentoteoricoequipo7.php"><input type="button" value="FUNDAMENTO TEORICO CAPITULO 7"></a>
+</ul>
+</center>
+</body>
+</html>
