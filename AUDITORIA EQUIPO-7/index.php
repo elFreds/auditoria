f218f3d4b1a25e47891b26144c29f8486fbe79eb
xavisa <j-lavis@hotmail.com>
2015-12-13 17:57:00 -0600

    Update index.php

--- AUDITORIA EQUIPO-7/index.php
+++ AUDITORIA EQUIPO-7/index.php
@@ -6,17 +6,22 @@
     <link rel="stylesheet" type="text/css" href="css/css.css"/>
  </head>
 <body style=”text-align:justify;”>
-
+<MARQUEE BEHAVIOR=alternate WIDTH=70%>
+<FONT FACE=arial COLOR=###### SIZE=8>
+INTERPRETACION DE LA INFORMACION
+</MARQUEE>
+<hr>
+<br><br>
 <!-- Menú de navegación del sitio -->
-<center>
+
 <ul>
    <li><a href="infocap3.php"><input type="button" value="INFORMACION SOBRE CAPITULO 3"></a>   
    <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 4"></a>
    <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 5"></a>
    <li><a href=""><input type="button" value="INFORMACION SOBRE CAPITULO 6"></a>
-   <li><a href="dictamenfinal.php"><input type="button" value="DICTAMEN FINAL></a>
+   <li><a href="dictamenfinal.php"><input type="button" value="DICTAMEN FINAL"></a>
    <li><a href="fundamentoteoricoequipo7.php"><input type="button" value="FUNDAMENTO TEORICO CAPITULO 7"></a>
 </ul>
-</center>
+
 </body>
 </html>
