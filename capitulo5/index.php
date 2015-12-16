2ef66062b70d7947968826a39982dd293a74831d
danylara <mynus_destructor@hotmail.com>
2015-12-16 08:47:08 -0600

    Dany
    subo fundamento teorico del capitulo5

---
+++ capitulo5/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>CAPITULO 5</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+
+	<script src="js/jquery-1.4.1.min.js" type="text/javascript"></script>
+	<script src="js/jquery.jcarousel.pack.js" type="text/javascript"></script>
+	<script src="js/jquery-func.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div id="bg-y">
+	<div id="bg-top">
+		<div id="top">
+			
+			
+			<div class="shell">
+				
+				
+				<div id="header">
+					<h1 id="logo"><a href="#">CAPITULO 5 Evaluacion del proceso de datos y de los equipos de computo <br /></a></h1>
+					<div id="navigation">
+						<ul>
+						    <li><a href="index.html">Capitulo 5</a></li>
+						    <li><a href="controles.html">Controles</a></li>
+						    <li><a href="orden.html">Orden</a></li>
+						    <li><a href="evaluacion.html">Evaluacion</a></li>
+						    <li><a href="productividad.html">Productividad</a></li>
+						</ul>
+					</div>
+				</div>
+				
+				<div id="main">
+				
+					<div class="post" id="home">
+						<h2>Introduccion</h2>
+						<p>Los datos son uno de los recursos mas valiosos de las organizaciones y, aunque son intangibles, necesitan ser controlados y auditados con el mismo cuidado que los demás inventarios de la organización, por lo cual se debe tener presente: </p>
+ <P>
+a)	La responsabilidad de los datos es compartida conjuntamente por alguna función determinada de la organización y la dirección de informática. </p>
+<p>
+b)	Un problema que se debe considerar es el que se origina por la duplicidad de los datos y consiste en poder determinar los propietarios o usuarios posibles (principalmente en el caso de redes y banco de datos) y la responsabilidad de su actualización y consistencia.</P>
+<p> 
+c)	Los datos deberán tener una clasificación estándar y un mecanismo de identificación que permita detectar duplicidad y redundancia dentro de una aplicación y de todas las aplicaciones en general. </p>
+<p>
+d)	Se deben relacionar los elementos de los datos con las bases de datos donde están </p>
+
+<p>
+e)	almacenados, así como los reportes y grupos de procesos donde son generados. 
+</p>
+					</div>				
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
