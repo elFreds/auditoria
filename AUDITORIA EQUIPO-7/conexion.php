301786332d4e406f873f45f0e16c1cd2c6ad81d0
xavisa <j-lavis@hotmail.com>
2016-01-24 23:54:17 -0600

    Update conexion.php

--- AUDITORIA EQUIPO-7/conexion.php
+++ AUDITORIA EQUIPO-7/conexion.php
@@ -1,4 +1,4 @@
 <?php  
-$connexion=mysql_connect("localhost","root","") or die('ERROR AL CONECTAR CON EL SERVIDOR'); 
-mysql_select_db("auditoria",$connexion)or die('ERROR AL CONECTAR CON LA BASE DE DATOS');
+$connexion=mysql_connect("localhost","taguevar_123","Hola123*") or die('ERROR AL CONECTAR CON EL SERVIDOR'); 
+mysql_select_db("taguevar_auditoria",$connexion)or die('ERROR AL CONECTAR CON LA BASE DE DATOS');
 ?>
