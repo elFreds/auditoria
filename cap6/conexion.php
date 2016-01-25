aa8af174595af188169993ea2c9cc1134a681479
EveLoz <eve_pvmas@hotmail.com>
2016-01-25 16:11:57 -0600

    conexion

--- cap6/conexion.php
+++ cap6/conexion.php
@@ -1,4 +1,4 @@
 <?php  
-$connexion=mysql_connect("67.222.148.10","taguevar_123","Hola123*") or die('ERROR AL CONECTAR CON EL SERVIDOR'); 
+$connexion=mysql_connect("localhost","taguevar_123","Hola123*") or die('ERROR AL CONECTAR CON EL SERVIDOR'); 
 mysql_select_db("taguevar_auditoria",$connexion)or die('ERROR AL CONECTAR CON LA BASE DE DATOS');
 ?>
